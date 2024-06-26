86d3c8290252bf3eb5b89479698246bec0dbe458
dti-jlo <Jonathan.loquet@epc-groupe.com>
2024-06-26 14:53:43 +0200

    toto tata titi

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
+int toto()
+{
+    return 42;
+}
+
+int tata()
+{
+    return 42;
+}
+
+int titi()
+{
+    return 42;
+}
+
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
